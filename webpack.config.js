18b4fd4f489586d02e430a214fcb42361c20cefe
Fedor <fedoravdeev3@gmail.com>
2023-07-02 21:16:22 +0700

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+module.exports = {
+  mode: 'none',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '/')
+    }
+  },
+  resolve: {
+    alias: {
+      three: path.resolve('./node_modules/three')
+    },
+    extensions: ['.tsx', '.ts', '.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
